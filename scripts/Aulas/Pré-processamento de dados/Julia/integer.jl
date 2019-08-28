d38774b7196eaca80bd0c9cf6e8102e254b541a0
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2019-08-28 17:05:38 -0300

    First Julia commit (incomplete)
    - Notebook (incomplete)
    - Scripts (incomplete)

---
+++ scripts/Aulas/Pré-processamento de dados/Julia/integer.jl
@@ -0,0 +1,5 @@
+# INTEGER
+
+qtdFilhos = 1
+typeof(qtdFilhos) # Retorna o tipo da variável
+print(qtdFilhos) # Printa o valor da variável no console
---
+++ scripts/Aulas/Pré-processamento de dados/Julia/lists.jl
@@ -0,0 +1,10 @@
+# LISTS
+# Um conjunto de valores que pode serheterogêneo.
+
+pesosV = [7.76, 80.3, 90, 117.4] # Cria um vetor dinâmico com tamanho variado
+nomesV = ["Maria", "Carlos", "Pedro", "Antônio"] # Cria um vetor dinâmico com tamanho variado
+
+myList = [pesosV, nomesV] # Cria um vetor dinâmico com tamanho variado com dois vetores como valores
+print(myList[1], myList[2]) # Printa os valores do vetor no console
+print(myList[1]) # Printa apenas os valores do primeiro vetor (pesosV) que está dentro do vetor myList
+print(myList[2]) # Printa apenas os valores do segundo vetor (nomesV) que está dentro do vetor myList
